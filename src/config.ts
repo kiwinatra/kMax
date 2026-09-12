cf9359f1eb5e7b5fb9d7203592981d56adc573e9
kiwinatra <mcshonax@gmail.com>
2026-09-12 20:43:02 +0500

    1.3.5 -> 1.4

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ import { OFFSETS } from './offsets';
 
 export const CONFIG = {
     name: 'kMax Mod',
-    version: '1.3.5',
+    version: '1.4',
     author: 'kiwinatra потемкин короче',
     site: 'max.ru',
 } as const;
